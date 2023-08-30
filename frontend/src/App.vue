c84592721113590c34c65f6f819d4dbf5a087a6b
RidhaaGabier <2811rg@gmail.com>
2023-08-30 16:34:13 +0200

    first commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+   <div>
+    <nav>
+      <NavBarComp/>
+    </nav>
+    <main>
+      <router-view/>
+    </main>
+    <footer>
+      <FooterComp/>
+    </footer>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+/* Hide scrollbar for Chrome, Safari and Opera */
+body::-webkit-scrollbar {
+  display: none;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+/* Hide scrollbar for IE, Edge and Firefox */
+body{
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
 }
 </style>
+
+
+<script>
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+export default {
+  components: {
+    NavBar,
+    Footer
+  }
+  
+}
+</script>
